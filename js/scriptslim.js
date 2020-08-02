9a691dfb40b8470432e7e7cc89bac66e01d5768b
Blackbird17922se <blackbird17922@gmail.com>
2020-08-02 17:14:17 -0500

    slidernuevo

---
+++ js/scriptslim.js
@@ -0,0 +1,22 @@
+$(function() {
+	var rangePercent = $('[type="range"]').val();
+	$('[type="range"]').on('change input', function() {
+		rangePercent = $('[type="range"]').val();
+		$('h4').text(rangePercent);
+		$('[type="range"]').css('filter', 'hue-rotate(-' + rangePercent + 'deg)');
+	});
+});
+
+// nuevo sl
+
+var rangeSlider = document.getElementById("rs-range-line");
+var rangeBullet = document.getElementById("rs-bullet");
+
+rangeSlider.addEventListener("input", showSliderValue, false);
+
+function showSliderValue() {
+  rangeBullet.innerHTML = rangeSlider.value;
+  var bulletPosition = (rangeSlider.value /rangeSlider.max);
+  rangeBullet.style.left = (bulletPosition * 578) + "px";
+}
+
